9a6a1ed08bc1aa72435b83b56b6788a4e91a83d8
JulioCesarAntonyFraga <juliocesaradefraga@gmail.com>
2021-03-13 02:08:36 -0300

    troca da rota inicial

--- AgendaJaque/Startup.cs
+++ AgendaJaque/Startup.cs
@@ -81,7 +81,7 @@ namespace AgendaJaque
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Pacientes}/{action=Index}/{id?}");
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
                 endpoints.MapRazorPages();
             });
         }
